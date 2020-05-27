8ddd7eea25662f253b73a0b83dabcc63c3bc820e
ayushhagarwal <ayushgrwl365@gmail.com>
2020-05-27 21:34:10 +0530

    Navbar Completed

---
+++ src/Components/Navbar/Navbar.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSignInAlt,faSignOutAlt } from '@fortawesome/free-solid-svg-icons'
+import '../Navbar/Navbar.css'
+
+export class Navbar extends Component {
+    render() {
+        return (
+            <div className="Navbar">
+                <div className="Navbar-Brand"><p>ToDo</p></div>
+                <div className="Navbar-Links">
+                    <a href="#" className="Navbar-Link"><FontAwesomeIcon icon={faSignInAlt} size='1.5x'/> Login</a>
+                    <a href="#" className="Navbar-Link"><FontAwesomeIcon icon={faSignOutAlt} size='1.5x'/>Signout</a>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Navbar
